24d71b3336c3e4019c5d05c3717fe3c3b45a5705
Unknown <lowell.list@gmail.com>
2018-02-16 11:45:06 -0800

    loaded custom font

--- App.js
+++ App.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import { StyleSheet, Image, Text, View } from 'react-native';
+import { Font } from 'expo';
 
 export default class App extends React.Component {
+
+  state = {
+    fontLoaded: false,
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'open-sans-regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+    });
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
-    return <View style={styles.splash_screen}>
-      <View style={styles.top_container}>
-        <Image source={require('./img/directors-logo.jpg')}></Image>
-        <Text>Open up App.js to start working on your app!</Text>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}}/>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+    return(
+      <View style={{flex: 1}}>
+        <View style={styles.top_container}>
+          <Image source={require('./assets/images/directors-logo.jpg')}></Image>
+          <Text>Open up App.js to start working on your app!</Text>
+          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}}/>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+        <View style={styles.bottom_container}>
+          {
+            (this.state.fontLoaded ? (
+              <Text style={{color: 'white', fontSize: 25, fontFamily: 'open-sans-regular'}}>HELLO LOWELL :)</Text>
+            ) : null)
+          }
+        </View>
       </View>
-      <View style={styles.bottom_container}>
-        <Text>HELLO LOWELL :)</Text>
-      </View>
-    </View>;
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  splash_screen: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'powderblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   top_container: {
     flex: 5,
     flexDirection: 'column',
@@ -36,7 +48,7 @@ const styles = StyleSheet.create({
   bottom_container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#ff77AA',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
