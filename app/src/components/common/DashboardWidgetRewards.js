3abf69280925825170f4fc4f08ffa1be59708d0f
Unknown <lowell.list@gmail.com>
2018-02-20 10:32:06 -0800

    added Dashboard buttons; hid status bar

--- app/src/components/common/DashboardWidgetRewards.js
+++ app/src/components/common/DashboardWidgetRewards.js
@@ -16,11 +16,11 @@ export default class DashboardWidgetRewards extends React.Component {
           <Text style={{color: DIRECT_GRAY, fontSize: 18, fontFamily: 'open-sans-regular'}}>{this.props.name}</Text>
         </View>
         <View style={{flexDirection: 'row', alignItems: 'flex-start', justifyContent:'center', backgroundColor: 'white', paddingHorizontal:10, paddingBottom: 10}}>
-          <View style={{flex: 1, flexDirection: 'column', alignItems:'center', backgroundColor: 'black', marginEnd: 5}}>
+          <View style={{flex: 1, flexDirection: 'column', alignItems:'center', backgroundColor: 'black', marginEnd: 5, padding: 3}}>
             <Text style={{color: 'white', fontSize: 22, fontFamily: 'open-sans-regular'}}>{this.props.effectiveSavings}</Text>
             <Text style={{color: 'white', fontSize: 8, fontFamily: 'open-sans-regular', marginTop: -5}}>effective savings to date</Text>
           </View>
-          <View style={{flex: 1, flexDirection: 'column', alignItems:'center', backgroundColor: 'black', marginStart: 5}}>
+          <View style={{flex: 1, flexDirection: 'column', alignItems:'center', backgroundColor: 'black', marginStart: 5, padding: 3}}>
             <Text style={{color: 'white', fontSize: 22, fontFamily: 'open-sans-regular'}}>{this.props.remainingSavings}</Text>
             <Text style={{color: 'white', fontSize: 8, fontFamily: 'open-sans-regular', marginTop: -5}}>remaining savings over life of loan</Text>
           </View>
--- app/src/components/screens/Dashboard.js
+++ app/src/components/screens/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Image, ImageBackground, ScrollView, Text, View} from 'react-native';
+import {StatusBar, Alert, Image, ImageBackground, ScrollView, Text, TouchableOpacity, View} from 'react-native';
 import {DIRECT_GRAY, DIRECT_TAN, Styles} from "../../constants/Styles";
 import DashboardWidgetAccount from "../common/DashboardWidgetAccount";
 import DashboardWidgetRewards from "../common/DashboardWidgetRewards";
@@ -22,6 +22,7 @@ export default class Dashboard extends React.Component {
   render() {
     return(
       <View style={[Styles.screen, {backgroundColor: DIRECT_GRAY}]}>
+        <StatusBar hidden />
         <ImageBackground
           style={{flex:1, alignItems: 'center', justifyContent: 'center'}}
           source={require('app/assets/images/blue-sky-water-and-green-grass.jpg')}
@@ -39,24 +40,44 @@ export default class Dashboard extends React.Component {
             : null
           }
         </ImageBackground>
-        <Text style={{color: 'white', fontSize: 25, fontFamily: 'open-sans-regular', alignSelf: 'center'}}>HELLO {this.props.name.toUpperCase()}</Text>
+        <Text style={{alignSelf: 'center', color: 'white', fontSize: 25, fontFamily: 'open-sans-regular'}}>HELLO {this.props.name.toUpperCase()}</Text>
         <View style={{flex:2}}>
           <ScrollView
-            style={{paddingHorizontal: 10, marginBottom: 30}}
+            style={{paddingHorizontal: 10, marginBottom: 50}}
             contentContainerStyle={{alignItems:'center'}}
           >
             <DashboardWidgetAccount name='Direct Checking' visibleAccountDigits='4357' dollars='$ 7,289.' cents='42'/>
             <DashboardWidgetAccount name='Direct Savings' visibleAccountDigits='2616' dollars='$ 14,347.' cents='23'/>
-            <DashboardWidgetAccount name='BofA Checking' visibleAccountDigits='1234' dollars='$ 8,420.' cents='58'/>
             <DashboardWidgetRewards name='Direct Rewards' effectiveSavings='$3,000.00' remainingSavings='$37,000.00'/>
           </ScrollView>
         </View>
-        <View style={{bottom: 5, width: '100%', alignItems: 'center', position: 'absolute'}}>
-          <View style={{width: 50, height: 50, borderRadius:25, backgroundColor: DIRECT_TAN, alignItems: 'center', justifyContent: 'center'}}>
-            <Text style={{color: 'white', fontSize: 25, fontFamily: 'open-sans-regular'}}>+</Text>
-          </View>
+        <View style={{position: 'absolute', alignSelf: 'center', bottom: 5, width: '75%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-end'}}>
+          <TouchableOpacity onPress={this._onPressHomeButton}>
+            <Image source={require('app/assets/images/icon-house.png')} style={{width: 40, height: 40}}/>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._onPressAddButton}>
+            <View style={{width: 50, height: 50, borderRadius:25, backgroundColor: DIRECT_TAN, alignItems: 'center', justifyContent: 'center'}}>
+              <Text style={{color: 'white', fontSize: 25, fontFamily: 'open-sans-regular'}}>+</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._onPressDirectorsButton}>
+            <Image source={require('app/assets/images/icon-directors.png')} style={{width: 40, height: 40}}/>
+          </TouchableOpacity>
         </View>
       </View>
     );
   }
+
+  _onPressHomeButton() {
+    Alert.alert('Home button pressed');
+  }
+
+  _onPressAddButton() {
+    Alert.alert('Add account button pressed');
+  }
+
+  _onPressDirectorsButton() {
+    Alert.alert('Directors button pressed');
+  }
+
 }
